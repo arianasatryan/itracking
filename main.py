ea4b4029dc64c1df6b29d645fbd7b0c762619a05
arianasatryan <arianasatryan@gmail.com>
2024-04-05 19:33:16 +0400

    added processing backend

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import os
-from fastapi import FastAPI, File, UploadFile, Request
+from fastapi import FastAPI, File, Form, UploadFile, Request
 from fastapi.responses import HTMLResponse, Response
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-app = FastAPI()
 
-templates = Jinja2Templates(directory=".")
+
+app = FastAPI()
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+templates = Jinja2Templates(directory=".")
 
 
 @app.post("/upload/image")
-async def upload_image(file: UploadFile = File(...)):
+async def upload_image(file: UploadFile = File(...), activeModel: str = Form(...)):
     file_content = await file.read()
     return Response(content=file_content, media_type="image/jpeg")
 
+
 @app.post("/upload/video")
-async def upload_video(file: UploadFile = File(...)):
+async def upload_video(file: UploadFile = File(...), activeModel: str = Form(...)):
+    print(activeSwitch, flush=True)
     file_content = await file.read()
     return Response(content=file_content, media_type="video/mp4")
 
---
+++ utils.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+import cv2
+
+models = {
+    "face": YOLO('models/yolov8n-face.pt'),
+    "pose": YOLO(f"yolov8x-pose.pt")
+}
+
+def detect(image_path, category, output_path, show_keypoints=True, show_boxes=True):
+    assert category in models
+    model = models[category]
+    results = model.predict(source=image_path, conf=0.3, classes=0, stream=False, verbose=False)
+    pil_image = results[0].plot(boxes=show_boxes, kpt_radius=int(show_keypoints)*3, pil=True, save=True, filename=output_path)
+    return pil_image, results
+
+
+def track(video_path, category, output_video_path, show_keypoints=True, show_boxes=True):
+    assert category in models
+    model = models[category]
+    cap = cv2.VideoCapture(video_path)
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    out = cv2.VideoWriter(output_video_path, fourcc, fps, (frame_width, frame_height))
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if success:
+            results = model.track(frame, persist=True)
+            annotated_frame = results[0].plot(boxes=show_boxes, kpt_radius=int(show_keypoints)*3)
+            out.write(annotated_frame)
+        else:
+            break
+    return output_video_path
