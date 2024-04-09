182ce5b87c9776b438f4884782823345b5286e2d
ariana <arianasat@ispras.ru>
2024-04-10 02:13:54 +0400

    Chane some visual

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './assets/styles/index.css'; 
 import './assets/styles/player.css'; 
+import './assets/styles/loading.css'; 
 import ImageUploader from './ImageUploader';
 import VideoUploader from './VideoUploader';
 import ModelOptions from './ModelOptions';
@@ -11,6 +12,7 @@ function App() {
   const [activeModel, setActiveModel] = useState('Face'); // Set default model to 'Face'
   const [activeOptions, setActiveOptions] = useState(['Boxes', 'Keypoints']); // Set both options checked by default
   const [uploadedImage, setUploadedImage] = useState(null);
+  const [loading, setLoading] = useState(false); // Define loading state here
 
   const handleModelChange = (event) => {
     setActiveModel(event.target.value);
@@ -19,18 +21,17 @@ function App() {
   return (
     <div className="container">
       <div className="uploaders">
-        <ImageUploader activeModel={activeModel} activeOptions={activeOptions} setUploadedImage={setUploadedImage} />
+        <ImageUploader activeModel={activeModel} activeOptions={activeOptions} setUploadedImage={setUploadedImage} setLoading={setLoading} />
         <VideoUploader activeModel={activeModel} activeOptions={activeOptions} />
         <div className="configurations">
           <ModelOptions selectedModel={activeModel} handleModelChange={handleModelChange} />
           <DisplayOptions setActiveOptions={setActiveOptions} />
         </div>
       </div>
-      {uploadedImage && (
-        <div className="uploaded-image-container">
-          <img src={uploadedImage} alt="Uploaded" className="uploaded-image" />
-        </div>
-      )}
+      <div className="uploaded-image-container">
+        {loading && <div className="loading-spinner"></div>}
+        {uploadedImage && !loading && <img src={uploadedImage} alt="Uploaded" className="uploaded-image" />}
+      </div>
     </div>
   );
 }
